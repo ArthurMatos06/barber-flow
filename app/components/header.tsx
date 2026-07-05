b39923bcb7396fad36666e1ffd278cccfd57fec1
arthur <arthurmatospessoal01@gmail.com>
2026-07-05 11:40:31 -0300

    chore: add Header

---
+++ app/components/header.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+export default function header() {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
