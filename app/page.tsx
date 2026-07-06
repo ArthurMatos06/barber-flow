513bdd8e7b50ddd8898c360e0ce945900b5f48c4
Arthur Matos <arthurmatospessoal01@gmail.com>
2026-07-06 10:05:51 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Button } from "./components/ui/button"
 import { SearchIcon } from "lucide-react"
 import { Input } from "./components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./components/ui/card"
+import { Badge } from "./components/ui/badge"
+import { Avatar, AvatarImage } from "./components/ui/avatar"
 const page = () => {
   return (
     <div>
@@ -28,6 +31,28 @@ const page = () => {
             className="rounded-xl object-cover"
           />
         </div>
+        {/*Agendamento*/}
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            {/*esquerda*/}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2"></div>
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p className="text-sm">Barbearia teste</p>
+            </div>
+            {/*direita*/}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">13:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
