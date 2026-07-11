41b8a0c1dc67c5b4deaf362214775850a5e0e933
Arthur Matos <arthurmatospessoal01@gmail.com>
2026-07-11 01:33:11 -0300

    feat: add initial barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { db } from "@/app/_lib/prisma"
 import { Button } from "@/app/components/ui/button"
-import { ChevronLeftIcon, MenuIcon } from "lucide-react"
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 type Props = {
   params: Promise<{
@@ -21,17 +22,13 @@ export default async function BarbershopPage({ params }: Props) {
 
   // Evita os erros de null
   if (!barbershop) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-xl font-bold">Barbearia não encontrada.</h1>
-      </div>
-    )
+    return notFound()
   }
 
   return (
     <div>
       {/* Imagem */}
-      <div className="relative h-[250px] w-full">
+      <div className="relative h-62.5 w-full">
         <Image
           alt={barbershop.name}
           src={barbershop.imageUrl}
@@ -56,6 +53,22 @@ export default async function BarbershopPage({ params }: Props) {
         >
           <MenuIcon />
         </Button>
+      </div>
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-6 text-xl font-bold">{barbershop.name}</h1>
+        <div className="mb-3 flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-muted-foreground text-sm">{barbershop?.address}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-muted-foreground text-sm">5,0 (138 avaliações)</p>
+        </div>
+      </div>
+      {/* descrição */}
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold text-gray-400 uppercase">Sobre nós</h2>
+        <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
     </div>
   )
