2501bc7a3a8eca52144d777fb183caa5e5851fbf
arthur <arthurmatospessoal01@gmail.com>
2026-07-13 16:52:55 -0300

    feat: add MobileMenu component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,24 +1,52 @@
+"use client"
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import Sidebar from "./SideBar"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog"
+import { useState } from "react"
 
-export default function header() {
+export default function Header() {
+  const [sheetOpen, setSheetOpen] = useState(false)
+  const [dialogOpen, setDialogOpen] = useState(false)
   return (
-    <Card>
-      <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-        <Sheet>
-          <SheetTrigger>
-            <Button size="icon" variant="outline">
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-          <Sidebar />
-        </Sheet>
-      </CardContent>
-    </Card>
+    <>
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-5">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+          <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+            <SheetTrigger>
+              <Button size="icon" variant="outline">
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+            <Sidebar
+              onLogin={() => {
+                setSheetOpen(false)
+                setDialogOpen(true)
+              }}
+            />
+          </Sheet>
+        </CardContent>
+      </Card>
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Faça login na plataforma</DialogTitle>
+            <DialogDescription>
+              Conecte-se usando sua conta do Google.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
