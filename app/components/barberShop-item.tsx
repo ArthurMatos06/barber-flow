cc9ed321b73d5ff90bba0a3f82c1157b4b192a63
arthur <arthurmatospessoal01@gmail.com>
2026-08-22 11:08:35 -0300

    refactor: improve responsive layout

--- app/components/barberShop-item.tsx
+++ app/components/barberShop-item.tsx
@@ -5,99 +5,51 @@ import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
 import Link from "next/link"
-import ShowOnMobile from "../components/ShowOnMobile"
-import ShowOnDesktop from "../components/ShowOnDesktop"
 
 interface BarberShopItemProps {
   barberShop: Barbershop
 }
 
 const BarberShopItem = ({ barberShop }: BarberShopItemProps) => {
-  //dps colocar um props class name para poder resolver o problema de size na pagina barbershops
   return (
-    <>
-      <ShowOnMobile>
-        <Card className="w-full min-w-41.75 rounded-2xl border bg-black/20 backdrop-blur-sm">
-          <CardContent className="p-0 px-1 pt-1">
-            {/* {imagem} */}
-            <div className="relative h-39.75 w-full">
-              <Image
-                fill
-                alt={barberShop.name}
-                sizes="'100vw"
-                className="rounded-2xl object-cover"
-                src={barberShop.imageUrl}
-              />
-
-              <Badge
-                className="border-gray/05 absolute top-2 left-2 space-x-1 bg-black/30 backdrop-blur-xs"
-                variant="secondary"
-              >
-                <StarIcon size={12} className="fill-primary text-primary" />
-                <p className="text-xs font-semibold">5,0</p>
-              </Badge>
-            </div>
-            {/* {texto} */}
-            <div className="px-1 py-3">
-              <h3 className="truncate font-semibold">{barberShop.name}</h3>
-              <p className="truncate text-sm text-gray-400">
-                {barberShop.description}
-              </p>
-              <Link
-                href={`/barbershops/${barberShop.id}`}
-                className="mt-3 block"
-              >
-                <Button variant="secondary" className="w-full">
-                  Reservar
-                </Button>
-              </Link>
-            </div>
-          </CardContent>
-        </Card>
-      </ShowOnMobile>
-      <ShowOnDesktop>
-        <Card className="border-gray/20 w-full min-w-70 rounded-2xl border bg-black/20 backdrop-blur-sm">
-          <CardContent className="p-0 px-3 pt-2">
-            {/* Imagem */}
-            <div className="relative h-56 w-full">
-              <Image
-                fill
-                alt={barberShop.name}
-                sizes="100vw"
-                className="rounded-2xl object-cover"
-                src={barberShop.imageUrl}
-              />
-
-              <Badge
-                className="absolute top-3 left-3 space-x-1 bg-black/30 backdrop-blur-xs"
-                variant="secondary"
-              >
-                <StarIcon size={14} className="fill-primary text-primary" />
-                <p className="text-sm font-semibold">5,0</p>
-              </Badge>
-            </div>
-
-            {/* Texto */}
-            <div className="px-2 py-4">
-              <h3 className="truncate text-lg font-semibold">
-                {barberShop.name}
-              </h3>
-
-              <p className="mt-1 truncate text-base text-gray-400">
-                {barberShop.description}
-              </p>
-
-              <Link
-                href={`/barbershops/${barberShop.id}`}
-                className="mt-4 block"
-              >
-                <Button className="h-11 w-full">Reservar</Button>
-              </Link>
-            </div>
-          </CardContent>
-        </Card>
-      </ShowOnDesktop>
-    </>
+    <Card className="border-gray/20 w-full min-w-41.75 rounded-2xl border bg-black/20 backdrop-blur-sm lg:min-w-70">
+      <CardContent className="p-0 px-1 pt-1 lg:px-3 lg:pt-2">
+        {/* Imagem */}
+        <div className="relative h-39.75 w-full lg:h-56">
+          <Image
+            fill
+            alt={barberShop.name}
+            sizes="100vw"
+            className="rounded-2xl object-cover"
+            src={barberShop.imageUrl}
+          />
+          <Badge
+            className="absolute top-2 left-2 space-x-1 bg-black/30 backdrop-blur-xs lg:top-3 lg:left-3"
+            variant="secondary"
+          >
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold lg:text-sm">5,0</p>
+          </Badge>
+        </div>
+        {/* Texto */}
+        <div className="px-1 py-3 lg:px-2 lg:py-4">
+          <h3 className="truncate font-semibold lg:text-lg">
+            {barberShop.name}
+          </h3>
+          <p className="truncate text-sm text-gray-400 lg:mt-1 lg:text-base">
+            {barberShop.description}
+          </p>
+          <Link
+            href={`/barbershops/${barberShop.id}`}
+            className="mt-3 block lg:mt-4"
+          >
+            <Button variant="secondary" className="w-full lg:h-11">
+              Reservar
+            </Button>
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 export default BarberShopItem
